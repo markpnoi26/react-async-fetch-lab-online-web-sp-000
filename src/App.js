1fb3ddd8255521b1f4d7e72a5590fced699c2986
Mark Daniel  Delgado <markpnoi26@gmail.com>
2020-01-02 17:14:32 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
 // create your App component here
+import React from 'react'
+
+export default class App extends React.Component {
+  
+  componentDidMount() {
+    fetch('http://api.open-no')
+  }
+  
+  render() {
+    return(
+      <h1>I am in the app now </h1>  
+    )
+  }
+  
+}
